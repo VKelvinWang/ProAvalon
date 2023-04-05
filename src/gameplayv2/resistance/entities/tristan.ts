f35071f274ff505ac114859e0e2c5860a5bc110c
asif-hf <akramasif923@yahoo.com>
2023-04-05 17:00:13 +1000

    Addition of files & their file directory

--- src/gameplayv2/resistance/entities/tristan.ts
+++ src/gameplayv2/resistance/entities/tristan.ts
@@ -1,9 +1,3 @@
 import { Vote } from "../components/vote.h";
-import { See } from "../components/see.h";
-//Extract components
 
-
-
-export abstract class Tristan{
-   //Tristan class
-}
+//Create Tristan class
