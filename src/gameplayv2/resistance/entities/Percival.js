aa70d2f2c9c0053121b86e96c134d4b2f801586f
WHITEDesu97 <sheffieldmzy97@gmail.com>
2023-04-09 16:11:36 +0800

    Update Percival.js

--- src/gameplayv2/resistance/entities/Percival.js
+++ src/gameplayv2/resistance/entities/Percival.js
@@ -1,5 +1,27 @@
-import { alliance } from  '../components/alliance'
-import { world } from '../system'
+import { createWorld, System } from 'ecsy';
+import { AllianceComponent } from './alliance_component';
+import { RoleComponent } from './role_component';
+import { DescriptionComponent } from './description_component';
+
+class Percival {
+  constructor() {
+    this.components = [];
+    this.addComponent(new AllianceComponent('Resistance'));
+    this.addComponent(new RoleComponent('Percival'));
+    this.addComponent(new DescriptionComponent('A member of the Resistance who knows the identity of Merlin and Morgana.'));
+  }
+
+  addComponent(component) {
+    this.components.push(component);
+  }
+
+  removeComponent(component) {
+    const index = this.components.indexOf(component);
+    if (index !== -1) {
+      this.components.splice(index, 1);
+    }
+  }
+}
 
 // Get the entity representing the player playing as Percival
 const percivalEntity = world.getEntities().find(entity => {
