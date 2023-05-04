7927a70c32ea8af90115ae8dcb2d84d5655735a9
asif-hf <akramasif923@yahoo.com>
2023-05-04 19:54:52 +1000

    Assassin

--- src/gameplay_v2/roles/assassin.ts
+++ src/gameplay_v2/roles/assassin.ts
@@ -1,6 +1,7 @@
 import Entity from './entity';
 import { Alliance } from '../gameTypes';
 import { assassinateC } from './components/assassinate';
+import { SeeSpiesC } from './components/seeSpies';
 
 export default class Assassin extends Entity {
   description: string;
@@ -8,6 +9,7 @@ export default class Assassin extends Entity {
   constructor() {
     super(Alliance.Resistance);
     this.description = 'If the resistance win 3 missions, the Assassin can shoot one person for Merlin, or two people for Tristan and Isolde. If they are correct, the spies win!';
-    this.components.push(new assassinateC());  
+    this.components.push(new assassinateC()); 
+    this.components.push(new SeeSpiesC()); 
   }
 }
--- src/gameplayv2/resistance/components/SeeAssassinComponent.ts
+++ src/gameplayv2/resistance/components/SeeAssassinComponent.ts
@@ -1,1 +1,44 @@
+<<<<<<< Updated upstream
 //TO DO 
+=======
+//TO DO 
+export class SeeAssassinComponent {
+    private Assassin: string;
+    private Role: string;
+    private isEnabled: boolean;
+
+    constructor(Assassin: string, Role: string, isEnabled: boolean) {
+        this.Assassin = Assassin;
+        this.Role = Role;
+        this.isEnabled = isEnabled;
+    }
+
+    // Getter and setter for Morgana
+    getMordred(): string {
+        return this.Assassin;
+    }
+
+    setMordred(Assassin: string): void {
+        this.Assassin = Assassin;
+    }
+
+    // Getter function for Role
+    getRole(): string {
+        return this.Role;
+    }
+
+    // Setter function for Role
+    setRole(Role: string): void {
+        this.Role = Role;
+    }
+
+    // Getter and setter for isEnabled
+    getIsEnabled(): boolean {
+        return this.isEnabled;
+    }
+
+    setIsEnabled(isEnabled: boolean): void {
+        this.isEnabled = isEnabled;
+    }
+}
+>>>>>>> Stashed changes
